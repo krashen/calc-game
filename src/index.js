3387f9c502dd3f474528c2f2293f4164742627b2
krashen <pablo.ferreyra.cxs@gmail.com>
2022-07-05 02:14:51 -0300

    clean

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from "react-redux";
 import { persistor, store } from "./store/index";
 import { PersistGate } from 'redux-persist/integration/react';
-import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
